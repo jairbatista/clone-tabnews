f1547af5f35b8e7923059b6c5c2191c06242cb87
Jair Batista <jairbatista@gmail.com>
2025-04-12 22:48:28 -0300

    Teste vercel

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,7 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1;");
-  console.log(result.rows);
-  response.status(200).json({ chave: "são acima da média" });
+  response.status(200).json({});
 }
 
 export default status;
